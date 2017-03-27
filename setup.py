fd19b5383bc304b614a165c54e9c31a0de1cbbba
Tayyeb <tayyeb@tsaze.com>
2017-03-27 09:36:26 +0430

    bump version to 1.4.3

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='validate_email_wt',
-    version='1.4.2',
+    version='1.4.3',
     py_modules=('validate_email',),
     author='w.Tayyeb',
     author_email='w.tayyeb@gmail.com',
